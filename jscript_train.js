49d4fb43ac1c57afc6c0c0b5ee7b82cbd77949b8
David Mollert <David Mollert>
2017-04-04 16:27:10 -0400

    Just a few final changes needed

--- jscript_train.js
+++ jscript_train.js
@@ -8,56 +8,69 @@ var config = {
   messagingSenderId: "162456693707"
 };
 firebase.initializeApp(config);
-
+// Working varilables are initialized
 var currentTime = "";
 var destination = "";
 var startTime = "";
 var frequency = "";
-
-currentTime = moment().format("HH:mm");
-$("#time").text(currentTime);
-
-var currentTimeMinute = moment.duration(currentTime).asMinutes();
-console.log(currentTimeMinute);
-var beginTime = "02:35";
-var beginTimeMinute = moment.duration(beginTime).asMinutes();
-console.log(beginTimeMinute);
-var theTime = currentTimeMinute - beginTimeMinute;
-console.log(theTime);
-var hr = theTime / 60;
-hr = parseInt(hr);
-var mn = theTime % 60;
-mn = parseInt(mn);
-console.log(hr+":"+mn);
-
+var nextArrival = "";
+var minutesAway = "";
+// Function to get the current time and place into HTML
+function getCurrentTime() {
+  currentTime = moment().format("HH:mm");
+  $("#time").text(currentTime);
+};
+// Function that calculates the Next Arrival and the Minutes Away
+function commingSoon() {
+  var currentTimeMinute = moment.duration(currentTime).asMinutes();
+  var startTimeMinute = moment.duration(startTime).asMinutes();
+  var theTimeMinutes = currentTimeMinute - startTimeMinute;
+  var usedMinutes = theTimeMinutes % frequency;
+  usedMinutes = parseInt(usedMinutes);
+  minutesAway = frequency - usedMinutes;
+  nextArrival = currentTimeMinute + minutesAway;
+  var hr = nextArrival / 60;
+  hr = parseInt(hr);
+  if (hr === 0) {
+    hr = 00;
+  }
+  else if (hr > 0 && hr < 10) {
+    hr = "0" + hr;
+  }
+  var mn = nextArrival % 60;
+  mn = parseInt(mn);
+  if (mn === 0) {
+    mn = 00;
+  }
+  else if (mn > 0 && mn < 10){
+    mn = "0" + mn;
+  }
+  nextArrival = hr + ":" + mn;
+};
+// Click event - Submit button which loads varibles and creates firebase entry
 $("#add-train").on("click", function(event) {
   event.preventDefault();
+  getCurrentTime();
+  commingSoon();
 	destination = $("#destination-input").val().trim();
 	startTime = $("#start-time-input").val().trim();
 	frequency = $("#frequency-input").val().trim();
-
-	console.log(destination);
-	console.log(startTime);
-	console.log(frequency);
+  currentTime = $("#frequency-input").val().trim();
 
   firebase.database().ref().push({
   	destination:destination,
   	startTime:startTime,
-  	frequency:frequency
+  	frequency:frequency,
+    nextArrival:nextArrival,
+    minutesAway:minutesAway
   });
 });
-
+// Firebase function which retrives entry and places them into HTML
 firebase.database().ref().on("child_added", function(snapshot) {
-  $("#destination").append("<p>"+snapshot.val().destination+"</p>");
-  $("#arrival-time").append("<p>"+snapshot.val().destination+"</p>");
-  $("#minutes-away").append("<p>"+snapshot.val().destination+"</p>");
-  $("#trip-time").append("<p>"+snapshot.val().frequency+"</p>");
+  $("#destination").append("<p>" + snapshot.val().destination + "</p>");
+  $("#arrival-time").append("<p>" + snapshot.val().nextArrival + "</p>");
+  $("#minutes-away").append("<p>" + snapshot.val().minutesAway + "</p>");
+  $("#trip-time").append("<p>" + snapshot.val().frequency + "</p>");
 });
-
-//firebase.database().ref().on("value", function(snapshot) {
-//  $("#destination").html(snapshot.val().destination);
-//  $("#arrival-time").html(snapshot.val().destination);
-//  $("#minutes-away").html(snapshot.val().destination);
-//  $("#trip-time").html(snapshot.val().frequency);
-//});
-
+// Function to get current time
+getCurrentTime();
